4012a93848612175e042c17c3d5b80d076d4c48a
Matthew Vosters <mattvosty@gmail.com>
2024-02-11 19:41:12 -0600

    Adds lots of level logic, beginning of suit, matching logic

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,10 +1,9 @@
 extends Sprite2D
 
-
+signal Chosen(inside)
 
 var inside = "hi"
 @export var outline_width = 10.0
-
 
 #Set up function, called when created by the level scene
 func setup(internal):
@@ -20,19 +19,22 @@ func _process(delta):
 	pass
 	
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed && get_rect().has_point(event.position):
-			print("Click: " + inside)
-	elif event is InputEventMouseMotion:
-		if get_rect().has_point(event.position):
-			print("Look at me")
-			highlight()
-		else:
-			dehighlight()
 
 func highlight(): #Effectively turns the shader outline on
 	material.set_shader_parameter("width", outline_width)
 	
 func dehighlight(): #Effectively turns the shader outline off
 	material.set_shader_parameter("width", 0.0)
+
+
+func _on_area_2d_mouse_entered():
+	highlight()
+
+func _on_area_2d_mouse_exited():
+	dehighlight()
+	
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton && event.pressed:
+		Chosen.emit(inside)
---
+++ scripts/match_suits.gd
@@ -0,0 +1,3 @@
+extends Object
+
+enum MATCH_SUITS { CLUBS, SPADES , HEARTS, DIAMONDS }
