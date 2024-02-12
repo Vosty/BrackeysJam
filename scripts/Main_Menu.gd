13cc8dbdaadd5b61152a4d9cd164a64d8df4c7d1
Austin Jensen <ausjens17@Hotmail.com>
2024-02-12 15:10:38 -0600

    Added main menu background

--- scripts/Main_Menu.gd
+++ scripts/Main_Menu.gd
@@ -22,3 +22,9 @@ func _on_quit_pressed():
 	#Exits the Game
 	get_tree().quit()
 	
+
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level.tscn")
+	pass # Replace with function body.
