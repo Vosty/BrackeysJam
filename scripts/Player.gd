11ed484af14dc396e806a88bbf15f4b72aabaeb1
Matthew Vosters <mattvosty@gmail.com>
2024-02-16 13:06:20 -0600

    Door open and close sounds, door and peeking feel improvements

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -28,6 +28,8 @@ var free_rerolls : int = 0
 var trap_avoid : float = 0.0
 var nearby_show : int = 0
 
+var is_peeking = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
